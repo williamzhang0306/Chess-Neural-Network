2677e4964fa7563512b3fb96cfc433a16b63f7a9
williamzhang0306 <williamzhang0306@gmail.com>
2022-12-30 10:03:32 -0600

    Reorganized Engine Classes

--- lichess-bot/strategies.py
+++ lichess-bot/strategies.py
@@ -8,7 +8,9 @@ import chess
 from chess.engine import PlayResult
 import random
 from engine_wrapper import EngineWrapper
-
+import sys
+from EngineClasses_copy import *
+count = 0
 print('imports done')
 
 model = models.load_model
@@ -109,161 +111,17 @@ class FirstMove(ExampleEngine):
         return PlayResult(moves[0], None)
 
 
-class NeuralNetEngine(ExampleEngine):
+class NegaMaxEngine(MinimalEngine):
     def __init__(self, commands, options, stderr, draw_or_resign, name=None, **popen_args):
         super().__init__(commands, options, stderr, draw_or_resign, name, **popen_args)
-        try:
-            self.model = models.load_model("/Users/williamzhang/Documents/College/chess project testing/lichess-bot/Model_3_12_16_22")
-            print("model loaded")
-        except:
-            print("MODEL FAILED TO LOAD")
-            raise ValueError
+        self.simple_engine = SimpleChessEngine(board = None, depth = 4)
 
-    def board_to_NN_input(self,board):
-        '''Creates a np array that can be used as input for the model from a chess.Board object'''
-        result = np.zeros([1,7,8,8])
-        for i in range(0,8):
-            for j in range(0,8):
-                square_index = 8*i + j
-                square = chess.SQUARES[square_index]
-                piece = board.piece_at(square)
-                # there is no piece move on to the next square
-                if piece == None: continue
-                square_value = 1 if piece.color == chess.WHITE else -1
-                piece_index = piece.piece_type
-                result[0][piece_index][i][j] = square_value
-        if board.turn == chess.WHITE:
-            result[0][0][0][4] = 1
-        else:
-            result[0][0][7][4] = 1
-        fen_info = board.fen().split(' ')
-        castling_rights = fen_info[2]
-        en_passant = fen_info[3]
-        # got the next 12 lines of code from kaggle: user UTA-DCM0927
-        castling = {
-            'K': (7,7),
-            'Q': (7,0),
-            'k': (0,7),
-            'q': (0,0),
-        }
-        if castling_rights != '-':
-            for char in castling_rights:
-                result[0, 0, castling[char][0], castling[char][1] ] = 1
-        if en_passant != '-':
-            result[0,0,int(en_passant[1]) - 1, ord(en_passant[0]) - ord('a')] = 1
-        return result
+    def search(self, board, time_limit, ponder, draw_offered, root_moves):
+        self.simple_engine.board = board
+        result = self.simple_engine.get_move()
+        move = result[0]
+        eval = result[1]
+        num_searches = result[2]
 
-    def search(self, board, *args):
-        print('searching')
-        
-        moves = list(board.legal_moves)        
-        # try:
-        #     model = models.load_model("Model_2_12_16_22")
-        #     print("model load succesfully")
-        # except:
-        #     model = None
-        #     print('model failed to load hm')
-        evaluations = []
-
-        for move in moves:
-            board.push(move)
-            try:
-                model_input = self.board_to_NN_input(board)
-            except:
-                model_input = np.zeros([1,7,8,8])
-            evaluations.append(self.model.predict(model_input)[0][0])
-            board.pop()
-
-
-        if board.turn == chess.WHITE:
-            tmp = max(evaluations)
-        else:
-            tmp = min(evaluations)
-            
-        index = evaluations.index(tmp)
-        best_move = moves[index]
-        print(best_move,tmp)
-        return PlayResult(best_move, None)
-
-class NeuralNetEngine2(ExampleEngine):
-
-    def __init__(self, commands, options, stderr, draw_or_resign, name=None, **popen_args):
-        print('NN Engine 2 created!')
-        super().__init__(commands, options, stderr, draw_or_resign, name, **popen_args)
-
-        try:
-            self.model = models.load_model("/Users/williamzhang/Documents/College/chess project testing/lichess-bot/Model_Cloud5_12_20_22")
-            print("Cloud Trained Model loaded")
-        except:
-            print("MODEL FAILED TO LOAD")
-            raise ValueError
-
-    def fen_to_image(self, fen_string: str) -> np.array:
-        '''Converts fen string to 8x8x12 (rows x cols x channels) image. 
-        13 channels represent 12 pieces plue one channel for white or black is playing.
-        In each pixel of the image, 1 represents a white piece is present, 
-        -1 black piece present, 0 no piece present.'''
-        # used for conversion
-        piece_to_channel = {'P':0, 'N':1, 'B':2, 'R':3 , 'Q':4 , 'K':5,
-                          'p':6, 'n':7, 'b':8, 'r':9, 'q':10, 'k':11}
-        image = np.zeros([8,8,12])
-
-        # extract info from the fen string
-        parts = fen_string.split(" ")
-        fen_board = parts[0].split('/')
-        #active_color = parts[1]
-        # populate the board image
-        for i, row in enumerate(fen_board):
-            j = 0
-            for char in row:
-                # if the character a piece, e.g. 'p' or 'K'
-                if char in piece_to_channel:
-                    # 1 if the piece is white, -1 if the piece is black
-                    val = 1 if char.isupper() else -1
-                    image[i, j, piece_to_channel[char]] = val
-                    j += 1
-
-                # otherwise the character is a number (number of empty squares in the row)
-                else:
-                    j += int(char)
-
-        out = np.empty([1,8,8,12])
-        out[0,] = image
-
-        return out
-
-    def search(self, board, *args):
-        print('searching')
-        
-        moves = list(board.legal_moves)        
-        evaluations = []
-
-        for move in moves:
-            board.push(move)
-            try:
-                model_input = self.fen_to_image(board.fen())
-                print('input conversion succeded')
-            except:
-                model_input = np.zeros([1,8,8,12])
-                print('input conversion failed')
-            evaluations.append(self.model.predict(model_input)[0][0])
-            board.pop()
-
-
-        if board.turn == chess.WHITE:
-            tmp = max(evaluations)
-        else:
-            tmp = min(evaluations)
-            
-        index = evaluations.index(tmp)
-        best_move = moves[index]
-        print(best_move,tmp)
-        return PlayResult(best_move, None)
-
-class NNEngine_with_Depth(NeuralNetEngine2):
-
-    def __init__(self, commands, options, stderr, draw_or_resign, name=None, **popen_args):
-        super().__init__(commands, options, stderr, draw_or_resign, name, **popen_args)
-
-    def serach(self, board, *args):
-        raise NotImplementedError
+        print(move,eval,num_searches)
+        return PlayResult(move,None)
