759b1928cba4b5956e804f0f6e16a104c85f4aeb
williamzhang0306 <williamzhang0306@gmail.com>
2022-12-31 13:54:12 -0600

    Cleaned up uncommented code

--- cloud-training/TrainPkg/TrainingScript.py
+++ cloud-training/TrainPkg/TrainingScript.py
@@ -3,12 +3,16 @@ from TrainPkg.MyDataHandler import DataGenerator
 import pandas as pd
 import os
 
-### read, parition and load data generators
-def main():
 
-    data = pd.read_csv("gs://chess-ai-bucket/keras-job-dir/chessData.csv")
+def main(cloud = True, model_type = 'CNN',model_name = 'Model_local_10_12_31_22'):
+
+    ### read, parition and load data generators
+    if cloud:  
+        data = pd.read_csv("gs://chess-ai-bucket/keras-job-dir/chessData.csv")
+    else:
+        data = pd.read_csv("/Users/williamzhang/Documents/College/Neural-Network-Chess/data/chessData.csv")
+    
     num_samples = len(data)
-
     partition_index = int( num_samples * 0.8 )
 
     partitioned_IDs = {
@@ -21,7 +25,6 @@ def main():
         'batch_size' : 256, 
         'x_dim' : (8,8,12),
         'y_dim' : (1,1),
-        'output_type' : 'normalized'
     }
 
     training_data = DataGenerator(IDs = partitioned_IDs['training'], **params)
@@ -29,38 +32,50 @@ def main():
 
     ### Create Model
     model = models.Sequential()
-    model.add(layers.InputLayer( input_shape = (8,8,12)))
-    model.add(layers.Flatten())
-    model.add(layers.Dense(768, activation = 'elu'))
-    model.add(layers.BatchNormalization())
-    model.add(layers.Dropout(0.2))
-    model.add(layers.Dense(768,activation= 'elu'))
-    model.add(layers.BatchNormalization())
-    model.add(layers.Dropout(0.2))
-    model.add(layers.Dense(350,activation= 'elu'))
-    model.add(layers.BatchNormalization())
-    model.add(layers.Dropout(0.2))
-    model.add(layers.Dense(175,activation= 'elu'))
-    model.add(layers.BatchNormalization())
-    model.add(layers.Dropout(0.2))
-    model.add(layers.Dense(50,activation= 'elu'))
-    model.add(layers.BatchNormalization())
-    model.add(layers.Dropout(0.2))
-    model.add(layers.Dense(1,activation='sigmoid'))
+
+    if model_type == 'MLP':
+        model.add(layers.InputLayer( input_shape = (8,8,12)))
+        model.add(layers.Flatten())
+        model.add(layers.Dense(768, activation = 'elu'))
+        model.add(layers.BatchNormalization())
+        model.add(layers.Dropout(0.2))
+        model.add(layers.Dense(768,activation= 'elu'))
+        model.add(layers.BatchNormalization())
+        model.add(layers.Dropout(0.2))
+        model.add(layers.Dense(350,activation= 'elu'))
+        model.add(layers.BatchNormalization())
+        model.add(layers.Dropout(0.2))
+        model.add(layers.Dense(175,activation= 'elu'))
+        model.add(layers.BatchNormalization())
+        model.add(layers.Dropout(0.2))
+        model.add(layers.Dense(50,activation= 'elu'))
+        model.add(layers.BatchNormalization())
+        model.add(layers.Dropout(0.2))
+        model.add(layers.Dense(1,activation='sigmoid'))
+
+    if model_type == 'CNN':
+        model.add(layers.Conv2D(filters=20, kernel_size=(5,5), activation='elu',data_format='channels_last', padding = 'same',input_shape = (8,8,12)))
+        model.add(layers.Dropout(0.3))
+        model.add(layers.BatchNormalization())
+        model.add(layers.Conv2D(filters=50, kernel_size=(3,3), activation='elu',data_format='channels_last', padding = 'same'))
+        model.add(layers.Dropout(0.3))
+        model.add(layers.BatchNormalization())
+        model.add(layers.Dense(units = 500, activation = 'linear'))
+        model.add(layers.Dropout(0.3))
+        model.add(layers.BatchNormalization())
+        model.add(layers.Flatten())
+        model.add(layers.Dense(units = 1, activation= 'sigmoid'))
+
     model.summary()
 
-    # Compile and Train Model
-    # model.compile(
-    #     optimizer=optimizers.Adam(learning_rate=0.001, beta_1=0.90, beta_2=0.99, epsilon= 1.91828183), 
-    #     loss = losses.mean_squared_error, 
-    #     metrics=['mean_absolute_error'])
+    ### Compile and Train Model
     model.compile(
         optimizer="adam",
         loss = losses.mean_squared_error,
         metrics = ['mean_absolute_error']
     )
 
-    es = callbacks.EarlyStopping(monitor='val_loss', mode='min', verbose=1, patience = 5)
+    es = callbacks.EarlyStopping(monitor='val_loss', mode='min', verbose=1, patience = 10)
 
     history = model.fit(training_data, 
                         epochs=200, 
@@ -71,12 +86,14 @@ def main():
                         callbacks= [es],
                         verbose = 2)
 
-    #model.save('Model_Cloud2_12_19_22')
+    if cloud:
+        export_path = os.path.join('gs://chess-ai-bucket/keras-job-dir', 'keras_export')
+    else:
+        export_path = os.path.join('/Users/williamzhang/Documents/College/Neural-Network-Chess/keras-exports')
 
-    export_path = os.path.join('gs://chess-ai-bucket/keras-job-dir', 'keras_export')
-    model.save(export_path+"/Model_Cloud7_12_29_22")
+    model.save(export_path+"/Model_Cloud9_12_29_22")
     print('Model exported to: {}'.format(export_path))
 
 
 if __name__ == "__main__":
-    main()
+    main(cloud = False, model_type='CNN')
